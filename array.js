ef731f0d8ec5202aceb7fb8d548576562cbf37a3
Harshad Shelke <153689497+harshadshelke041@users.noreply.github.com>
2025-10-28 03:02:35 +0000

    commit chanes in array.js

--- array.js
+++ array.js
@@ -6,7 +6,7 @@ let num = [1,2,3,4,5];
 
 let copy = num[0]
 
-for(let i=0;i<num.length;i++) {
+for(let i=0;i<num.length-1;i++) {
   num[i] = num[i + 1];
 }
 
@@ -15,7 +15,7 @@ num[num.length-1] = copy;
 console.log(num);
 
 
-// 2 -
+// 2 - 
 
 
 
