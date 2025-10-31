0263288db0db1e896991a9ea03377278cc820014
Harshad Shelke <153689497+harshadshelke041@users.noreply.github.com>
2025-10-31 08:01:14 +0530

    Update array.js

--- array.js
+++ array.js
@@ -15,7 +15,24 @@ num[num.length-1] = copy;
 console.log(num);
 
 
-// 2 - 
+// 2 - Right Push By 1 Element
+
+let num = [1,2,3,4,5];
+
+let copy = num[num.length -1]
+console.log(copy);
+
+
+
+
+for(let i=num.length-1;i>0;i--) {
+  num[i] = num[i-1]
+}
+
+num[0] = copy
+
+console.log(num);
+
 
 
 
