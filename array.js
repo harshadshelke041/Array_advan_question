d179e0912e246d27bacfe901c5aaa1eb2b44d2a0
Harshad Shelke <153689497+harshadshelke041@users.noreply.github.com>
2025-11-01 23:21:13 +0530

    Update array.js

--- array.js
+++ array.js
@@ -33,6 +33,52 @@ num[0] = copy
 
 console.log(num);
 
+// 3 - Left Rotation by k element
+
+let num = [1, 2, 3, 4, 5]
+
+let k = 2;
+
+k = k % arr.length  
+
+for (let i = 0; i < k; i++) {
+
+  let copy = num[0];
+
+  for (let j = 0; j < num.length - 1; j++) {
+
+    num[j] = num[j + 1]
+  }
+
+  num[num.length - 1] = copy
+}
+
+console.log(num);
+
+
+// 4- Right Rotation by k element
+
+  let num = [1, 2, 3, 4, 5];
+
+let k = 4;
+
+k = k % num.length;
+
+for (let i = 0; i < k; i++) {
+
+  let copy = num[num.length - 1];
+  console.log(copy);
+
+
+  for (let j = num.length - 1; j > 0; j--) {
+
+    num[j] = num[j - 1]
+  }
+
+  num[0] = copy
+}
+
+console.log(num);
 
 
 
