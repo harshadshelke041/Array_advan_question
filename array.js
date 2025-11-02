2f4871a2cd5792bcc3a9112cf0d1b5851cc842ea
Harshad Shelke <153689497+harshadshelke041@users.noreply.github.com>
2025-11-02 22:10:53 +0530

    Update array.js

--- array.js
+++ array.js
@@ -176,6 +176,30 @@ while (j < num2.length) {
 
 console.log(merge);
 
+// Best time to sell or buy Stock
+
+let prices = [7, 1, 5, 3, 6, 4];
+
+let min = prices[0]
+let maxProfit = 0;
+
+function profit(prices) {
+
+
+  for (let i = 0; i < prices.length; i++) {
+
+    if (prices[i] < min) {
+      min = prices[i]
+    }
+
+    let profit = prices[i] - min;
+    maxProfit = Math.max(profit, maxProfit)
+  }
+  return maxProfit
+}
+
+console.log(profit(prices)); 
+
 
 
 
